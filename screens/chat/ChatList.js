f61ff223beed9e2e20e1999bb1a97566c3dadc09
şöhrett <sohretturaman@gmail.com>
2023-07-25 16:41:04 +0300

    new users saved to firestore for chat

--- screens/chat/ChatList.js
+++ screens/chat/ChatList.js
@@ -1,37 +1,52 @@
-import {StyleSheet, Text, TextInput, View,SafeAreaView} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, TextInput, View, SafeAreaView} from 'react-native';
+import React, {useEffect} from 'react';
 import {Avatar, Divider, FAB, List} from 'react-native-paper';
 
 import {PaperProvider, Button, Dialog, Portal} from 'react-native-paper';
 import {useState} from 'react';
 import {ScrollView} from 'react-native-gesture-handler';
-import Firestore from '@react-native-firebase/firestore'
+import Firestore from '@react-native-firebase/firestore';
+import {firebase} from '@react-native-firebase/auth';
 
-
-
-function ChatList  () {
+function ChatList() {
   const [visible, setVisible] = useState(false);
-  const [frgnEmail,setFrgnEmail]=useState('sohrett@gmail.com');
-  const [myEmail,setMyEmail]=useState(''); 
+  const [frgnEmail, setFrgnEmail] = useState('');
+  const [myEmail, setMyEmail] = useState('');
 
   const showDialog = () => setVisible(true);
   const hideDialog = () => setVisible(false);
 
-  const HandleCreateChat =()=>{
-    Firestore().collection('chats').add({users:['firstemail@gmail.com','second@gmail.com']})
-    setVisible(false);
-  }
-  return (
-    <SafeAreaView style={{flex:1}}> 
-      <ScrollView >
-        <View>
+  const HandleCreateChat = () => {
+    console.log('my data',myEmail);
+    console.log('*******************');
+    console.log('and second data',frgnEmail);   
+    try {
+      if (frgnEmail && myEmail) {
+        Firestore()
+          .collection('chats')
+          .add({users: [myEmail,frgnEmail]});
+        setVisible(false);
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+    setFrgnEmail('');
+   
+  };
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setMyEmail(user?.email ?? '');
+    });
+  }, []);
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ScrollView>
+        <View>
           <List.Item
             title="First Item"
             description="Item description"
-            left={props => (
-              <List.Icon {...props} icon="folder" />
-            )}
+            left={props => <List.Icon {...props} icon="folder" />}
           />
           <Divider
             leftInset={40}
@@ -43,15 +58,18 @@ function ChatList  () {
             description="Item description"
             left={props => <List.Icon {...props} icon="folder" />}
           />
-       
-    
+
           {/**chat list , dialog componnet react  native paper */}
-          <Button onPress={showDialog}>Show Dialog</Button>
           <Portal>
             <Dialog visible={visible} onDismiss={() => setVisible(false)}>
               <Dialog.Title>Text by Email</Dialog.Title>
               <Dialog.Content>
-                  <TextInput  placeholder='write a user name to make a chat' value={frgnEmail} onChangeText={(value)=>setFrgnEmail(value)}/>
+                <TextInput
+                  placeholder="write a user name to make a chat"
+                  value={frgnEmail}
+                  onChangeText={value => setFrgnEmail(value)}
+                  style={{borderColor:'black',borderWidth:0.5}}
+                />
               </Dialog.Content>
               <Dialog.Actions>
                 <Button onPress={hideDialog}>Cancel</Button>
@@ -59,22 +77,18 @@ function ChatList  () {
               </Dialog.Actions>
             </Dialog>
           </Portal>
-
-          
-         
         </View>
-        </ScrollView>
+      </ScrollView>
 
       {/**FAB button for  creating new dialog */}
       <FAB
-            icon="plus"
-            style={{position: 'absolute', bottom: 16, right: 16}}
-            onPress={() => console.log('Pressed')}
-          />
-              
-      </SafeAreaView>
+        icon="plus"
+        style={{position: 'absolute', bottom: 16, right: 16}}
+        onPress={showDialog}
+      />
+    </SafeAreaView>
   );
-};
+}
 
 export default ChatList;
 

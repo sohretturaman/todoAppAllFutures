de92eec7840926bf17c50774fe1ecae55d435355
şöhrett <sohretturaman@gmail.com>
2023-07-26 15:21:16 +0300

    chatlist data retrieved from firestore

--- screens/chat/Chat.js
+++ screens/chat/Chat.js
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useRoute } from "@react-navigation/native";
 
 const Chat = () => {
+  const route = useRoute();
+  console.log('route',route.params.chatId);
+  
+
+  
   return (
     <View>
       <Text>Chat</Text>
--- screens/chat/ChatList.js
+++ screens/chat/ChatList.js
@@ -7,57 +7,84 @@ import {useState} from 'react';
 import {ScrollView} from 'react-native-gesture-handler';
 import Firestore from '@react-native-firebase/firestore';
 import {firebase} from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
 
 function ChatList() {
+  const navigation = useNavigation(); 
   const [visible, setVisible] = useState(false);
   const [frgnEmail, setFrgnEmail] = useState('');
   const [myEmail, setMyEmail] = useState('');
+  const [chatListData, setChatListData] = useState([]);
+  const[newChatLoading,setNewChatLoading]=useState(false); 
 
   const showDialog = () => setVisible(true);
   const hideDialog = () => setVisible(false);
 
-  const HandleCreateChat = () => {
-    console.log('my data',myEmail);
-    console.log('*******************');
-    console.log('and second data',frgnEmail);   
+  const HandleCreateChat = async () => {
+    // console.log('my data',myEmail);
+    // console.log('*******************');
+    // console.log('and second data',frgnEmail);
     try {
       if (frgnEmail && myEmail) {
-        Firestore()
+        setNewChatLoading(true); 
+       const result = await Firestore()
           .collection('chats')
-          .add({users: [myEmail,frgnEmail]});
+          .add({users: [myEmail, frgnEmail]});
         setVisible(false);
+         navigation.navigate('Chat',{resultId:result.id})
+         console.log('result id', result);
+         
       }
     } catch (error) {
       console.log('error', error);
     }
     setFrgnEmail('');
-   
+    setNewChatLoading(false); 
   };
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
       setMyEmail(user?.email ?? '');
     });
+  
   }, []);
+
+
+  useEffect(()=>{
+   firebase
+    .firestore()
+    .collection('chats')
+    .where('users', 'array-contains', myEmail)
+    .onSnapshot(onSnapShotData => {
+      setChatListData(onSnapShotData.docs);
+    });
+  },[myEmail])
+
+
+
+ // console.log('data ',chatListData.map((x)=>x.data().users));
+  
+  
   return (
     <SafeAreaView style={{flex: 1}}>
       <ScrollView>
         <View>
-          <List.Item
-            title="First Item"
-            description="Item description"
-            left={props => <List.Icon {...props} icon="folder" />}
-          />
-          <Divider
-            leftInset={40}
-            style={{backgroundColor: 'white', padding: 5}}
-          />
-
-          <List.Item
-            title="First Item"
-            description="Item description"
-            left={props => <List.Icon {...props} icon="folder" />}
-          />
+          {chatListData?.map((item,index) => {
+            return (
+              <View key={index}>
+                <List.Item
+                  title={item.data().users.filter(x=>x!==myEmail)}
+                  description="Item description"
+                  left={()=><Avatar.Text  size={50}  style={{marginLeft:5}} label={item.data().users.find(x=> x!==myEmail).split(' ').reduce((prev,current)=>prev+current[0],'')} />}
+               
+               onPress={()=>navigation.navigate('Chat',{chatId:item.id})}/>
+                <Divider
+                  
+                  style={{backgroundColor: 'white', padding:2}}
+                />
+              </View>
+            );
+          })}
 
           {/**chat list , dialog componnet react  native paper */}
           <Portal>
@@ -68,12 +95,12 @@ function ChatList() {
                   placeholder="write a user name to make a chat"
                   value={frgnEmail}
                   onChangeText={value => setFrgnEmail(value)}
-                  style={{borderColor:'black',borderWidth:0.5}}
+                  style={{borderColor: 'black', borderWidth: 0.5}}
                 />
               </Dialog.Content>
               <Dialog.Actions>
                 <Button onPress={hideDialog}>Cancel</Button>
-                <Button onPress={HandleCreateChat}>done</Button>
+                <Button onPress={HandleCreateChat} >done</Button>
               </Dialog.Actions>
             </Dialog>
           </Portal>
